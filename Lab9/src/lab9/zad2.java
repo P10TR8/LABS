b7f205fe7cf007e21935a2d8f4430ac5dec01d9e
P10TR8 <45950882+P10TR8@users.noreply.github.com>
2018-12-18 20:21:34 +0100

    Add files via upload

---
+++ Lab9/src/lab9/zad2.java
@@ -0,0 +1,18 @@
+package lab9;
+import java.time.LocalDate;
+import java.util.Random;
+public class zad2 extends Thread implements Runnable{
+int id_filmu;
+int id_u¿ytkownika;
+int ocena;
+LocalDate data_glosowania=LocalDate.of(2017, 12, 30);
+
+public void run()
+{
+	
+}
+public static void main(String[] args)
+{
+	
+}
+}
