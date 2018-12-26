e1e1eeb509deb87b440e00aff4164c8f5e2bd868
P10TR8 <45950882+P10TR8@users.noreply.github.com>
2018-12-26 13:44:45 +0100

    Add files via upload

--- Lab8/src/lab8/Test.java
+++ Lab8/src/lab8/Test.java
@@ -1,37 +1,74 @@
 package lab8;
 import java.util.Scanner;
-public class Test extends StudentInstytut{
-	Scanner odczyt = new Scanner(System.in);
-	String x;
-public String CheckString(String x)
-{
-	while(x == "")
-	{
-		System.out.println("Brak parametru! Podaj parametr.");
-		x=odczyt.next();
-		this.x=x;	
-		
-	}
-	return this.x;
-}
+import java.util.regex.*;
 
+public class Test {
 public static void main(String[] args)
 {
+	StudentInstytut s1 = new StudentInstytut();
 	Scanner odczyt = new Scanner(System.in);
-	
-	Test s1 = new Test();
 	System.out.println("Podaj imie: ");
-	String x = odczyt.next();
-	s1.CheckString(x);
-	System.out.println("x = "+x);
+	String imie=odczyt.next();
+	Pattern pattern = Pattern.compile("\\D");
+	Matcher matcher=pattern.matcher(imie);
+	while(matcher.find()==false)
+	{
+		System.out.println("Blad, wprowadz popprawne dane: ");
+		imie=odczyt.next();
+		matcher=pattern.matcher(imie);
+
+	}
+	System.out.println("Podaj nazwisko: ");
+	String nazwisko=odczyt.next();
+	matcher=pattern.matcher(nazwisko);
+	while(matcher.find()==false)
+	{
+		System.out.println("Blad, wprowadz popprawne dane: ");
+		nazwisko=odczyt.next();
+		matcher=pattern.matcher(nazwisko);
+
+	}
+	System.out.println("Podaj wydzial: ");
+	String wydzial=odczyt.next();
+	matcher=pattern.matcher(wydzial);
+	while(matcher.find()==false)
+	{
+		System.out.println("Blad, wprowadz popprawne dane: ");
+		wydzial=odczyt.next();
+		matcher=pattern.matcher(wydzial);
+
+	}
+	System.out.println("Podaj instytut: ");
+	String instytut=odczyt.next();
+	matcher=pattern.matcher(instytut);
+	while(matcher.find()==false)
+	{
+		System.out.println("Blad, wprowadz popprawne dane: ");
+		instytut=odczyt.next();
+		matcher=pattern.matcher(instytut);
+
+	}
 	
-	s1.UstawImie(x);
+	pattern = Pattern.compile("\\d");
+	System.out.println("Podaj pesel: ");
+	String pesel=odczyt.next();
+	matcher=pattern.matcher(pesel);
+	while(matcher.find()==false)
+	{
+		System.out.println("Blad, wprowadz popprawne dane: ");
+		pesel=odczyt.next();
+		matcher=pattern.matcher(pesel);
+
+	}
+	int pese=Integer.parseInt(pesel);
 	
-	s1.UstawNazwisko("Nowak");
-	s1.Ustaw_indeks(123);
-	s1.Ustaw_wydzial("WiEA");
-	s1.Ustaw_Instytut("Jakiœ instytut");
+	
+	s1.UstawImie(imie);
+	s1.UstawNazwisko(nazwisko);
+	s1.Ustaw_pesel(pese);
+	s1.Ustaw_wydzial(wydzial);
+	s1.Ustaw_Instytut(instytut);
 	s1.getInstytut();
-	System.out.println(s1.getInstytut()+s1.getImie());
+	System.out.println(s1.getInstytut()+" "+s1.getImie());
 }
 }
